b6b45248276a940de7465ae17a831616c2ffaea4
odethier-ulb <olivier.de.thier@ulb.be>
2024-12-24 16:07:17 +0100

    Add gene names, TODO: orientation

--- test_circular.py
+++ test_circular.py
@@ -1,8 +1,8 @@
 import drawsvg as draw
-from math import pi
+from math import pi, cos, sin
 from dataclasses import dataclass
 from typing import List, Tuple
-from mtSVG import Gene, MtGenome, Point, DrawableGenome, get_genomes
+from mtSVG import Gene, MtGenome, Point, DrawableGenome, get_genomes, get_clean_name
 
 
 
@@ -27,6 +27,7 @@ INTRA_GENOME_SPACE = 10
 GENE_HEIGHT = 160
 ORIENTATION_HEIGHT = 30
 SPECIES_HEIGHT = 80
+RADIUS_RATIO = 0.88
 
 RIBBON_HEIGHT = GENE_HEIGHT + ORIENTATION_HEIGHT + INTRA_GENOME_SPACE + INTER_GENOME_SPACE + SPECIES_HEIGHT
 
@@ -65,21 +66,56 @@ def draw_circular_genome(drawable: DrawableGenome, drawing: draw.Drawing):
     x_pos = 0
     for gene in drawable.genome.genes:
         x_pos = draw_circular_gene(drawable, gene, c_x, c_y, x_pos, r_in, r_out, drawing)
+
+
+# NEW
+def x_to_deg(x: float, radius: float) -> float:
+    angle = x / radius
+    angle = pi/2 - angle if angle <= pi/2 else 5*pi/2 - angle
+    return angle * (180 / pi)
+
+# NEW
+def x_to_polar(x: float, radius: float) -> Tuple[float, float]:
+    angle = (x / radius) - pi/2
+    return angle, radius * cos(angle), radius * sin(angle)
+    
         
 # NEW
 def draw_circular_gene(drawable: DrawableGenome, gene: Gene, c_x: float, c_y: float, 
                        x_pos: float, r_in: float, r_out: float, drawing: draw.Drawing) -> float:
-    # draw circular arc
-    angle_from = (x_pos / r_out) * (180 / pi)
-    angle_to = ((x_pos + gene.scaled_length * SCALE_FACTOR) / r_out) * (180 / pi)
-    angle_from = 90 - angle_from if angle_from <= 90 else 450 - angle_from
-    angle_to = 90 - angle_to if angle_to <= 90 else 450 - angle_to
+    # draw gene arcs
+    angle_from = x_to_deg(x_pos, r_out)
+    angle_to = x_to_deg(x_pos + gene.scaled_length * SCALE_FACTOR, r_out)
     drawing.append(draw.ArcLine(c_x, c_y, (r_in + r_out) / 2, angle_to, angle_from,
         stroke='black', stroke_width=RIBBON_HEIGHT/2 - STROKE_WIDTH, fill='none', fill_opacity=0.0))
     drawing.append(draw.ArcLine(c_x, c_y, (r_in + r_out) / 2, angle_to + 0.15, angle_from - 0.15,
         stroke=get_color(drawable.color_scheme, gene.name), stroke_width=RIBBON_HEIGHT/2 - STROKE_WIDTH, fill='none', fill_opacity=0.0))
-    # add the gene name
-    # TODO    
+
+    # draw gene name
+    gene_name = gene.name if drawable.full_name else get_clean_name(gene.name)
+    font_size = int(GENE_HEIGHT / 3.5)
+    gene_size = len(gene_name) * font_size / 2
+
+    if gene_size < gene.scaled_length * SCALE_FACTOR:
+        angle, origin_x, origin_y = x_to_polar(x_pos + ((gene.scaled_length * SCALE_FACTOR) - gene_size) / 2, r_out)
+        text_rotation = int((angle + pi/1.9) * (180/pi))
+        text_x, text_y = RADIUS_RATIO * origin_x + c_x, RADIUS_RATIO * origin_y + c_y
+        drawing.append(draw.Text(gene_name, font_size,text_x, text_y, font_family=drawable.font, 
+                                 transform=f'rotate({text_rotation}, {text_x}, {text_y})'))   
+    else:
+        if gene_size > GENE_HEIGHT:
+            font_size = int(2 * GENE_HEIGHT / len(gene_name))
+            gene_size = len(gene_name) * font_size / 2
+
+        angle, origin_x, origin_y = x_to_polar(x_pos + ((gene.scaled_length * SCALE_FACTOR) + font_size * 0.7) / 2, r_out)
+        text_x, text_y = (RADIUS_RATIO - .03) * origin_x + c_x, (RADIUS_RATIO - .03) * origin_y + c_y
+        text_rotation = int(angle * (180/pi))
+        drawing.append(draw.Text(gene_name, font_size,text_x, text_y, font_family=drawable.font, 
+                                 transform=f'rotate({text_rotation}, {text_x}, {text_y})'))
+
+    # draw orientation
+    # TODO 
+ 
     return x_pos + gene.scaled_length * SCALE_FACTOR
     
 
@@ -100,7 +136,7 @@ def draw_circle(genomes: List[MtGenome], output: str,
 
 # ----------------------------- TESTING -----------------------------
 
-gffs = [('Styela plicata', 14414, 'example/s_plicata.gff', False)]
+gffs = [('Halocynthia roretzi', 14771, 'example/h_roretzi.gff', False)]
 genomes = get_genomes(gffs, 'cox1', 0, False)
 draw_circle(genomes, 'test.svg', monochromatic=False, font='Arial', full_name=False, oriented=False)
 print('Done!')
